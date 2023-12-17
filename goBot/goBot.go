78aefbbee4fd7d0518f3b76a42c04e65ebcd2f1d
Gabriel Mazza <57022338+newcoder69@users.noreply.github.com>
2023-12-17 14:22:39 -0500

    Update goBot.go

--- goBot/goBot.go
+++ goBot/goBot.go
@@ -20,11 +20,16 @@ import (
 )
 var (
 	messageCount map[string]int
+	moneyCount map[string]int
+	userState map[string]string
+	
 )
 	
 
 func main(){
 	messageCount = make(map[string]int)
+	moneyCount = make(map[string]int)
+	userState = make(map[string]int)
 	//create the bot session
 	dg, err := discordgo.New("Bot insert your bot token here")
 	if(err != nil){
@@ -35,6 +40,15 @@ func main(){
 	dg.AddHandler(messageCreate)
 	dg.AddHandler(getStats)
 	dg.AddHandler(voiceStateUpdate)
+	dg.AddHandler(brainPower)
+	dg.AddHandler(MugMode)
+	dg.AddHandler(Troll)
+	dg.AddHandler(penisSize)
+	dg.AddHandler(coinFlip)
+	//dg.AddHandler(hydrationReminder)
+	dg.AddHandler(monkey)
+	dg.AddHandler(casinoEntrance)
+	dg.AddHandler(slotMachine)
 
 	err = dg.Open()
 	if err != nil {
@@ -177,3 +191,157 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate){
 		return nil
 	}
 	
+func MugMode(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "mug" {
+		s.ChannelMessageSend(m.ChannelID, "maniac")
+		embed := &discordgo.MessageEmbed{
+			Title:       "Mug Maniac",
+			Description: "hes going mug mode",
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://i5.walmartimages.com/seo/Mug-Root-Beer-Caffeine-Free-Soda-Pop-12-oz-24-Pack-Cans_4490f5a5-41ec-4f3b-9643-398eaeb0fd2c.10f661ed5389b6231c1b8026b76dbf30.jpeg?odnHeight=640&odnWidth=640&odnBg=FFFFFF",
+			},
+			Color: 0x3498db,
+		}
+		_, err := s.ChannelMessageSendEmbed(m.ChannelID, embed)
+		if err != nil {
+			fmt.Println("could not embed message")
+		}
+	}
+
+}
+func penisSize(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "!dicksize" {
+		chance := rand.Intn(100)
+		if chance <= 99 {
+			size := rand.Intn(12) + 1
+			dick := fmt.Sprintf("%s", strings.Repeat("=", size))
+
+			s.ChannelMessageSend(m.ChannelID, "your dick is this big"+" Ɛ"+dick+">")
+
+			if size <= 4 {
+				s.ChannelMessageSend(m.ChannelID, "thats pretty big home slice")
+			} else if size <= 8 {
+				s.ChannelMessageSend(m.ChannelID, "woah, nice cock. Thick but not too flacid")
+			} else if size <= 12 {
+				s.ChannelMessageSend(m.ChannelID, "gyatt damn he's packing")
+			} else {
+				s.ChannelMessageSend(m.ChannelID, "where is my dick at")
+			}
+
+		} else if chance >= 100 {
+			dick := "======================================================================================="
+			s.ChannelMessageSend(m.ChannelID, "SUPER MEGA GYTATT ALERT!! :siren: siren:\n Ɛ"+dick+">")
+		}
+
+	}
+
+}
+
+func coinFlip(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if strings.HasPrefix(m.Content, "!coinflip") {
+		result := rand.Intn(2) + 1
+
+		if result == 1 {
+			s.ChannelMessageSend(m.ChannelID, "you got heads :coin:")
+		} else if result == 2 {
+			s.ChannelMessageSend(m.ChannelID, "you got tails :coin:")
+		}
+
+	}
+}
+func monkey(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == "monkey" {
+		embed := &discordgo.MessageEmbed{
+
+			Title:       "monkey",
+			Description: "mmmm monkey",
+			Image: &discordgo.MessageEmbedImage{
+				URL: "https://media1.tenor.com/m/nd6rC1fcc3oAAAAC/effy.gif",
+			},
+			Color: 0x3498db,
+		}
+		s.ChannelMessageSendEmbed(m.ChannelID, embed)
+	}
+
+}
+
+func casinoEntrance(s *discordgo.Session, m *discordgo.MessageCreate) {
+	user := m.Author.ID
+
+	moneyCount[user] = 1000
+
+	if strings.HasPrefix(m.Content, "!wallet") {
+		s.ChannelMessageSend(m.ChannelID, "you have "+fmt.Sprint(moneyCount[user])+" dollars :dollar:")
+	}
+
+	if strings.HasPrefix(m.Content, "!casino") {
+		user_state[user] = "inside casino"
+		s.ChannelMessageSend(m.ChannelID, "Welcome to the casino! Your options are Slot Machine :slot_machine: (type !slotmachine), working on others after slot machine is finished")
+	}
+
+}
+
+func slotMachine(s *discordgo.Session, m *discordgo.MessageCreate) {
+	user := m.Author.ID
+	baseCost := 10
+	multiplier := 0.0
+
+	if strings.HasPrefix(m.Content, "!slotmachine") && user_state[user] == "inside casino" {
+		s.ChannelMessageSend(m.ChannelID, "Welcome to the slot machine! Test your luck \n it costs 10 coins to play plus you can bet for bigger rewards \n would you like to play?")
+		user_state[user] = "playing_slot"
+	} else if strings.HasPrefix(m.Content, "!slotmachine") && user_state[user] != "inside casino" {
+		{
+			s.ChannelMessageSend(m.ChannelID, "you're not in the casino silly goose. Welcome in!")
+			user_state[user] = "inside casino"
+		}
+
+	}
+	if user_state[user] == "playing_slot" && strings.HasPrefix(m.Content, "no") {
+		s.ChannelMessageSend(m.ChannelID, "okay have fun in the casino!")
+	} else if user_state[user] == "playing_slot" && strings.HasPrefix(m.Content, "yes") {
+		user_state[user] = "betting"
+		s.ChannelMessageSend(m.ChannelID, "awesome! the betting options are \n 0, \n 50, \n 100, \n 150, \n and 200")
+	}
+
+	if user_state[user] == "betting" && strings.HasPrefix(m.Content, "0") {
+		multiplier = 0
+		user_state[user] = "playing"
+		s.ChannelMessageSend(m.ChannelID, "alright bets are locked in. Ready to spin?")
+	} else if user_state[user] == "betting" && strings.HasPrefix(m.Content, "50") {
+		multiplier = .5
+		user_state[user] = "playing"
+		s.ChannelMessageSend(m.ChannelID, "alright bets are locked in. Ready to spin?")
+	} else if user_state[user] == "betting" && strings.HasPrefix(m.Content, "100") {
+		multiplier = 1.1
+		user_state[user] = "playing"
+		s.ChannelMessageSend(m.ChannelID, "alright bets are locked in. Ready to spin?")
+	} else if user_state[user] == "betting" && strings.HasPrefix(m.Content, "150") {
+		multiplier = 1.5
+		user_state[user] = "playing"
+		s.ChannelMessageSend(m.ChannelID, "alright bets are locked in. Ready to spin?")
+	} else if user_state[user] == "betting" && strings.HasPrefix(m.Content, "200") {
+		multiplier = 2.0
+		user_state[user] = "playing"
+		s.ChannelMessageSend(m.ChannelID, "alright bets are locked in. Ready to spin?")
+	}
+
+	if user_state[user] == "playing" && strings.HasPrefix(m.Content, "yes") {
+		rand := rand.Intn(100) + 1
+
+		if rand >= 1 && rand <= 10 {
+			s.ChannelMessageSend(m.ChannelID, "Sorry champ you lost, better luck next time. Try to get your money up and your funny down. :sad:")
+			loss := baseCost + (baseCost * int(multiplier))
+			moneyCount[user] = moneyCount[user] - float32(loss)
+		} else if rand >= 11 && rand <= 67 {
+			s.ChannelMessageSend(m.ChannelID, "Welp, you broke even. good job :sunglasses:")
+			moneyCount[user] = moneyCount[user]
+		} else if rand >= 68 && rand <= 99 {
+			s.ChannelMessageSend(m.ChannelID, "Congratulations, you win")
+			gain := baseCost + (baseCost * int(multiplier))
+			moneyCount[user] = moneyCount[user] + float32(gain)
+		}
+
+	}
+
+}
+
